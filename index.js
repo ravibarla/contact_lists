709a77365127082421ab1b49d45a2fe050c8ff15
ravi barla <ravibarla1@gmail.com>
2023-10-25 13:08:32 +0530

    sending data to the server

--- index.js
+++ index.js
@@ -19,6 +19,13 @@ app.get("/", (req, res) => {
 
     return res.render("home", { title: "my contact lists", contact_lists: contactList })
 })
+app.get("/practice", (req, res) => {
+
+    return res.render("practice", { title: "lets play with ejs" })
+})
+app.post("/create-contact", (req, res) => {
+    return res.redirect("/practice")
+})
 app.listen(port, (err) => {
     if (err) {
         console.log(err)
