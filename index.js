f8bf27324ac69fd53bd174282036d19ee8df7c8b
ravi barla <ravibarla1@gmail.com>
2023-10-25 12:58:18 +0530

    creating a contact lists

--- index.js
+++ index.js
@@ -5,9 +5,19 @@ const __dirname = path.resolve()
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
 const port = 3100
+var contactList = [
+    {
+        "name": "ravi",
+        "phone": 112233
+    },
+    {
+        "name": "akash",
+        "phone": 223344
+    }
+]
 app.get("/", (req, res) => {
 
-    return res.render("home")
+    return res.render("home", { title: "my contact lists", contact_lists: contactList })
 })
 app.listen(port, (err) => {
     if (err) {
