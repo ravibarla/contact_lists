0da7db9ef81e81df5fbd624bad24f2477f5dd8f1
ravi barla <ravibarla1@gmail.com>
2023-10-25 18:04:42 +0530

    fetching , deleting and adding contacts using mongoose query

--- index.js
+++ index.js
@@ -11,34 +11,44 @@ const port = 3100
 app.use(express.static("assets"))
 app.use(express.urlencoded())
 
-var contactList = [
-    {
-        "name": "ravi",
-        "phone": 112233
-    },
-    {
-        "name": "akash",
-        "phone": 223344
-    }
-]
+// var contactList = [
+//     {
+//         "name": "ravi",
+//         "phone": 112233
+//     },
+//     {
+//         "name": "akash",
+//         "phone": 223344
+//     }
+// ]
 app.get("/", (req, res) => {
-
-    return res.render("home", { title: "my contact lists", contact_lists: contactList })
+    Contact.find({}).then(contacts => {
+        return res.render("home", { title: "my contact lists", contact_lists: contacts })
+    }).catch(err => console.log("Error in fetching contacts"))
 })
 app.get("/practice", (req, res) => {
 
     return res.render("practice", { title: "lets play with ejs" })
 })
 app.post("/create-contact", (req, res) => {
-    contactList.push(req.body)
+    // contactList.push(req.body)
+    Contact.create({
+        name: req.body.name,
+        phone: req.body.phone
+    }).then(newContact => console.log("*******", newContact)).catch(err => console.log(err))
+
     return res.redirect("/")
 })
-app.get("/delete-contact/:phone", (req, res) => {
-    console.log(req.params)
-    let phone = req.params.phone
-    let updatedContactLists = contactList.filter(contact => contact.phone !== phone)
-    contactList = updatedContactLists
-    return res.redirect("back")
+app.get("/delete-contact/", (req, res) => {
+    //get the id from the url
+
+    let id = req.query.id
+    //find the contact in the database  using id and delete
+    Contact.findByIdAndDelete(id)
+        .then(success => res.redirect("back"))
+        .catch(err => { console.log("not able to delete from db"); return })
+
+    // return res.redirect("back")
 })
 app.listen(port, (err) => {
     if (err) {
